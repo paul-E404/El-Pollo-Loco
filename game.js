18c6e39a36308642adc4b6e1dca3dc99379c7ef4
paul-E404 <paul_engerling@web.de>
2021-03-16 17:56:14 +0100

    Started with implementation of caching

--- game.js
+++ game.js
@@ -14,11 +14,11 @@ let lastMove = "right";             //left or right
 let lastJumpStarted = new Date().getTime();
 let lastMoveFinished = new Date().getTime();
 let timePassedSinceJump;    //unter Vorbehalt. Ggf. wieder entfernen hier oben.  
-let currentCharacterImage = 'img/character/idle/Ir-1.png';
+let currentCharacterImage = new Image;
 let character_idle_index = 0;
 let character_walk_index = 0;
 
-let character_idle_right_images = [
+/* let character_idle_right_images = [
     'img/character/idle/Ir-1.png',
     'img/character/idle/Ir-2.png',
     'img/character/idle/Ir-3.png',
@@ -28,7 +28,7 @@ let character_idle_right_images = [
     'img/character/idle/Ir-7.png',
     'img/character/idle/Ir-8.png',
     'img/character/idle/Ir-9.png',
-    'img/character/idle/Ir-10.png',
+    'img/character/idle/Ir-10.png'
 ]
 
 let character_idle_left_images = [
@@ -41,7 +41,7 @@ let character_idle_left_images = [
     'img/character/idle/Il-7.png',
     'img/character/idle/Il-8.png',
     'img/character/idle/Il-9.png',
-    'img/character/idle/Il-10.png',
+    'img/character/idle/Il-10.png'
 ]
 
 let character_walk_right_images = [
@@ -50,7 +50,7 @@ let character_walk_right_images = [
     'img/character/walk/Wr-23.png',
     'img/character/walk/Wr-24.png',
     'img/character/walk/Wr-25.png',
-    'img/character/walk/Wr-26.png',
+    'img/character/walk/Wr-26.png'
 ]
 
 let character_walk_left_images = [
@@ -59,7 +59,7 @@ let character_walk_left_images = [
     'img/character/walk/Wl-23.png',
     'img/character/walk/Wl-24.png',
     'img/character/walk/Wl-25.png',
-    'img/character/walk/Wl-26.png',
+    'img/character/walk/Wl-26.png'
 ]
 
 let character_jump_right_images = [
@@ -84,7 +84,7 @@ let character_sleep_right_images = [
     'img/character/sleep/Sr-17.png',
     'img/character/sleep/Sr-18.png',
     'img/character/sleep/Sr-19.png',
-    'img/character/sleep/Sr-20.png',
+    'img/character/sleep/Sr-20.png'
 ]
 
 let character_sleep_left_images = [
@@ -97,20 +97,122 @@ let character_sleep_left_images = [
     'img/character/sleep/Sl-17.png',
     'img/character/sleep/Sl-18.png',
     'img/character/sleep/Sl-19.png',
-    'img/character/sleep/Sl-20.png',
+    'img/character/sleep/Sl-20.png'
 ]
 
 let character_hurt_right_images = [
     'img/character/hurt/Hr-41.png',
     'img/character/hurt/Hr-42.png',
-    'img/character/hurt/Hr-43.png',
+    'img/character/hurt/Hr-43.png'
 ]
 
 let character_hurt_left_images = [
     'img/character/hurt/Hl-41.png',
     'img/character/hurt/Hl-42.png',
-    'img/character/hurt/Hl-43.png',
-]
+    'img/character/hurt/Hl-43.png'
+] */
+
+//Objekt (Mihais Ansatz)
+let characterImages = {
+    walk: [
+        ['img/character/walk/Wl-21.png',
+            'img/character/walk/Wl-22.png',
+            'img/character/walk/Wl-23.png',
+            'img/character/walk/Wl-24.png',
+            'img/character/walk/Wl-25.png',
+            'img/character/walk/Wl-26.png'],
+        ['img/character/walk/Wr-21.png',
+            'img/character/walk/Wr-22.png',
+            'img/character/walk/Wr-23.png',
+            'img/character/walk/Wr-24.png',
+            'img/character/walk/Wr-25.png',
+            'img/character/walk/Wr-26.png']
+    ],
+    jump: [
+        ['img/character/jump/Jl-31.png',
+            'img/character/jump/Jl-32.png',
+            'img/character/jump/Jl-33.png'],
+        ['img/character/jump/Jr-31.png',
+            'img/character/jump/Jr-32.png',
+            'img/character/jump/Jr-33.png']
+    ],
+    hurt: [
+        ['img/character/hurt/Hl-41.png',
+            'img/character/hurt/Hl-42.png',
+            'img/character/hurt/Hl-43.png'],
+        ['img/character/hurt/Hr-41.png',
+            'img/character/hurt/Hr-42.png',
+            'img/character/hurt/Hr-43.png']
+    ],
+    idle: [
+        ['img/character/idle/Il-1.png',
+            'img/character/idle/Il-2.png',
+            'img/character/idle/Il-3.png',
+            'img/character/idle/Il-4.png',
+            'img/character/idle/Il-5.png',
+            'img/character/idle/Il-6.png',
+            'img/character/idle/Il-7.png',
+            'img/character/idle/Il-8.png',
+            'img/character/idle/Il-9.png',
+            'img/character/idle/Il-10.png'],
+        ['img/character/idle/Ir-1.png',
+            'img/character/idle/Ir-2.png',
+            'img/character/idle/Ir-3.png',
+            'img/character/idle/Ir-4.png',
+            'img/character/idle/Ir-5.png',
+            'img/character/idle/Ir-6.png',
+            'img/character/idle/Ir-7.png',
+            'img/character/idle/Ir-8.png',
+            'img/character/idle/Ir-9.png',
+            'img/character/idle/Ir-10.png']
+    ],
+    sleep: [
+        ['img/character/sleep/Sl-11.png',
+            'img/character/sleep/Sl-12.png',
+            'img/character/sleep/Sl-13.png',
+            'img/character/sleep/Sl-14.png',
+            'img/character/sleep/Sl-15.png',
+            'img/character/sleep/Sl-16.png',
+            'img/character/sleep/Sl-17.png',
+            'img/character/sleep/Sl-18.png',
+            'img/character/sleep/Sl-19.png',
+            'img/character/sleep/Sl-20.png'],
+        ['img/character/sleep/Sr-11.png',
+            'img/character/sleep/Sr-12.png',
+            'img/character/sleep/Sr-13.png',
+            'img/character/sleep/Sr-14.png',
+            'img/character/sleep/Sr-15.png',
+            'img/character/sleep/Sr-16.png',
+            'img/character/sleep/Sr-17.png',
+            'img/character/sleep/Sr-18.png',
+            'img/character/sleep/Sr-19.png',
+            'img/character/sleep/Sr-20.png']
+    ]
+};
+
+//#############################################################################################
+
+//Ziel dieser Funktion ist es, jeden Pfadeintrag des Objektes characterImages durch das entsprechende Bildobjekt zu ersetzen und es dadurch vorzuladen.
+//Gibt jedes Element des Objekts "characterImages" aus, d.h. den Pfad.
+//x ist die Anzahl der Elemente im Objekt
+//y ist die Wahl zwischen left und right
+//z ist die Anzahl der Elemente in einem spezifischen Array, z.B. 6 Elemente im Array walk-left (da dort 6 Bilder vorhanden).
+function preloadImages() {
+
+    for (let x = 0; x < Object.keys(characterImages).length; x++) {  
+        for(let y = 0; y < characterImages[Object.keys(characterImages)[x]].length; y++) {
+            for (let z = 0; z < characterImages[Object.keys(characterImages)[x]][y].length; z++) {
+                //Catch every entry (img-path) in characterImages object
+                let path = characterImages[Object.keys(characterImages)[x]][y][z];
+                console.log("Der Pfad ist: ", path);
+                let image = new Image();
+                image.src = path;
+                //Save entries with img-Objects back to characterImages object
+                characterImages[Object.keys(characterImages)[x]][y][z] = image;
+            }
+        }
+    }
+}
 
 
 // ------------------- Game config
@@ -119,24 +221,75 @@ let JUMP_TIME = 400;
 let GAME_SPEED = 7;
 let CLOUD_SPEED = 0.2;
 
+//Alte preloadFunktion. Kann gelöscht werden, sobald  die neue funktioniert.
+
+/* function preloadImages() {
+
+    for (let i = 0; i < imagePaths.length; i++) {
+        let image = new Image();
+        image.src = imagePaths[i];
+        imageCache.push(image); // push image-path to imageCache-array (which contains all image-paths)
+    }
+
+}
+
+function getImageFromCache(path) {
+
+    let image;
+
+    for (let i = 0; i < imageCache.length; i++) {
+        if (imageCache[i]['src'].endsWith(path) == true) {
+            image = imageCache[i];
+        };
+    }
+
+    return image;
+
+} */
+
 
 //Zeichnet das Spielfeld (Startpunkt links oben)
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
+    preloadImages();
     checkForRelaxing();
     checkForRunning();
     draw();
     listenForKeys();
 }
 
+function checkForRunning() {
+    setInterval(function () {
+        if (isMovingRight == true) {
+            let currentImages = characterImages['walk'][1];
+            showRunningImages(currentImages);
+        }
+        else if (isMovingLeft == true) {
+            let currentImages = characterImages['walk'][0];
+            showRunningImages(currentImages);
+        }
+    }, 100);
+}
+
+function showRunningImages(currentImages) {
+    //Modulorechnung sorgt dafür, dass der Index immer von 0 bis zur Länge des Arrays durch das Array iteriert.
+    //Der Counter am Ende setzt den Index immer +1 nach oben.
+    //if(timePassedSinceJump > JUMP_TIME * 2): Damit sich Sprung und Laufanimation nicht überlagern.
+    if (timePassedSinceJump > JUMP_TIME * 2) {
+        let index = character_walk_index % currentImages.length;
+        currentCharacterImage = currentImages[index];
+        character_walk_index++;
+    }
+
+}
+
 function checkForRelaxing() {
     setInterval(function () {
         let currentTime = new Date().getTime();
         let timePassedSinceLastMove = currentTime - lastMoveFinished;
-        console.log("timePassedSinceJump: ", timePassedSinceJump);
-        if(isMovingLeft == false && isMovingRight == false) {
-            if(timePassedSinceJump > JUMP_TIME * 3 && timePassedSinceJump <= JUMP_TIME * 20 || timePassedSinceLastMove > JUMP_TIME * 2 && timePassedSinceLastMove < JUMP_TIME * 20) {
+        if (isMovingLeft == false && isMovingRight == false) {
+            if (timePassedSinceJump > JUMP_TIME * 3 && timePassedSinceJump <= JUMP_TIME * 20 || timePassedSinceLastMove > JUMP_TIME * 2 && timePassedSinceLastMove < JUMP_TIME * 20) {
                 checkRelaxingDirection();
             }
             else if (timePassedSinceJump > JUMP_TIME * 3 && timePassedSinceLastMove > JUMP_TIME * 20) {
@@ -148,37 +301,37 @@ function checkForRelaxing() {
 
 function checkRelaxingDirection() {
     if (lastMove == "right") {
-        let character_idle_images = character_idle_right_images;
-        relax(character_idle_images);
+        let currentImages = characterImages['idle'][1];
+        relax(currentImages);
     }
     else {
-        let character_idle_images = character_idle_left_images;
-        relax(character_idle_images);
+        let currentImages = characterImages['idle'][0];
+        relax(currentImages);
     }
 }
 
-function relax(character_idle_images) {
-    let index = character_idle_index % character_idle_images.length;
-    currentCharacterImage = character_idle_images[index];
+function relax(currentImages) {
+    let index = character_idle_index % currentImages.length;
+    currentCharacterImage = currentImages[index];
     character_idle_index++;
 }
 
- 
+
 function checkSleepingDirection() {
     if (lastMove == "right") {
-        let character_sleep_images = character_sleep_right_images;
-        sleep(character_sleep_images);
+        let currentImages = characterImages['sleep'][1];
+        sleep(currentImages);
     }
     else {
-        let character_sleep_images = character_sleep_left_images;
-        sleep(character_sleep_images);
+        let currentImages = characterImages['sleep'][0];
+        sleep(currentImages);
     }
 }
 
 
-function sleep(character_sleep_images) {
-    let index = character_idle_index % character_sleep_images.length;
-    currentCharacterImage = character_sleep_images[index];
+function sleep(currentImages) {
+    let index = character_idle_index % currentImages.length;
+    currentCharacterImage = currentImages[index];
     character_idle_index++;
 }
 
@@ -194,34 +347,11 @@ function draw() {
     requestAnimationFrame(draw);
 }
 
-function checkForRunning() {
-    setInterval(function () {
-        if (isMovingRight == true) {
-            let character_walk_images = character_walk_right_images;
-            showRunningImages(character_walk_images);
-        }
-        else if (isMovingLeft == true) {
-            let character_walk_images = character_walk_left_images;
-            showRunningImages(character_walk_images);
-        }
-    }, 100);
-}
 
-function showRunningImages(character_walk_images) {
-    //Modulorechnung sorgt dafür, dass der Index immer von 0 bis zur Länge des Arrays durch das Array iteriert.
-    //Der Counter am Ende setzt den Index immer +1 nach oben.
-    //if(timePassedSinceJump > JUMP_TIME * 2): Damit sich Sprung und Laufanimation nicht überlagern.
-    if (timePassedSinceJump > JUMP_TIME * 2) {
-        let index = character_walk_index % character_walk_images.length;
-        currentCharacterImage = character_walk_images[index];
-        character_walk_index++;
-    }
-
-}
 
 function updateCharacter() {
-    character_image = new Image();
-    character_image.src = currentCharacterImage;
+    let character_image = currentCharacterImage;
+
     //character_image.complete: Gibt den Wert true zurück, wenn das Bild fertig geladen ist. Ansonsten false.
     if (character_image.complete) {
         ctx.drawImage(character_image, character_x, character_y, character_image.width * 0.35, character_image.height * 0.35);
@@ -267,10 +397,6 @@ function drawSky() {
         addBackgroundImage('clouds1_image', 'img/bg/sky/clouds1.png', bg_sky_x - cloud_offset, bg_element_y, i);
         addBackgroundImage('clouds2_image', 'img/bg/sky/clouds2.png', bg_sky_x - cloud_offset, bg_element_y, i + 1);
     }
-    /*     addBackgroundImage('sky_image', 'img/bg/sky/sky.png', bg_sky_x, bg_element_y, 0);
-        addBackgroundImage('sky_image', 'img/bg/sky/sky.png', bg_sky_x, bg_element_y, 1);
-        addBackgroundImage('sky_image', 'img/bg/sky/clouds1.png', bg_sky_x, bg_element_y, 0);
-        addBackgroundImage('sky_image', 'img/bg/sky/clouds2.png', bg_sky_x, bg_element_y, 1); */
 }
 
 function drawGround() {
@@ -286,23 +412,11 @@ function drawGround() {
         addBackgroundImage('ground1.1_image', 'img/bg/ground1/1.png', bg1_ground_x, bg_element_y, i);
         addBackgroundImage('ground1.2_image', 'img/bg/ground1/2.png', bg1_ground_x, bg_element_y, i + 1);
     }
-    /*  addBackgroundImage('ground3.1_image', 'img/bg/ground3/1.png', bg3_ground_x, bg_element_y, 0);
-     addBackgroundImage('ground3.2_image', 'img/bg/ground3/2.png', bg3_ground_x, bg_element_y, 1);
-     addBackgroundImage('ground3.1_image', 'img/bg/ground3/1.png', bg3_ground_x, bg_element_y, 2);
-     addBackgroundImage('ground3.2_image', 'img/bg/ground3/2.png', bg3_ground_x, bg_element_y, 3);
-     addBackgroundImage('ground2.1_image', 'img/bg/ground2/1.png', bg2_ground_x, bg_element_y, 0);
-     addBackgroundImage('ground2.2_image', 'img/bg/ground2/2.png', bg2_ground_x, bg_element_y, 1);
-     addBackgroundImage('ground2.1_image', 'img/bg/ground2/1.png', bg2_ground_x, bg_element_y, 2);
-     addBackgroundImage('ground2.2_image', 'img/bg/ground2/2.png', bg2_ground_x, bg_element_y, 3);
-     addBackgroundImage('ground1.1_image', 'img/bg/ground1/1.png', bg1_ground_x, bg_element_y, 0);
-     addBackgroundImage('ground1.2_image', 'img/bg/ground1/2.png', bg1_ground_x, bg_element_y, 1);
-     addBackgroundImage('ground1.1_image', 'img/bg/ground1/1.png', bg1_ground_x, bg_element_y, 2);
-     addBackgroundImage('ground1.2_image', 'img/bg/ground1/2.png', bg1_ground_x, bg_element_y, 3); */
 }
 
-function addBackgroundImage(name, src, bg_element_x, bg_element_y, scale) {
+function addBackgroundImage(name, path, bg_element_x, bg_element_y, scale) {
     name = new Image();
-    name.src = src;
+    name.src = path;
     if (name.complete) {
         ctx.drawImage(name, bg_element_x + canvas.width * scale, bg_element_y, canvas.width, canvas.height);
     }
@@ -335,6 +449,7 @@ function listenForKeys() {
         //JUMP_TIME * 2, da wir JUMP_TIME hochspringen und JUMP_TIME runterfallen (ein Sprung).
         if ((key == "Space" || key == "ArrowUp") && timePassedSinceJump > JUMP_TIME * 2) {
             lastJumpStarted = new Date().getTime();         //Unix Timestamp
+            timePassedSinceLastMove = new Date().getTime();
             checkJumpDirection();
 
         }
@@ -359,23 +474,23 @@ function listenForKeys() {
 
 function checkJumpDirection() {
     if (lastMove == "right") {
-        let character_jump_images = character_jump_right_images;
-        animateJump(character_jump_images);
+        let currentImages = characterImages['jump'][1];
+        animateJump(currentImages);
     }
     else {
-        let character_jump_images = character_jump_left_images;
-        animateJump(character_jump_images);
+        let currentImages = characterImages['jump'][0];
+        animateJump(currentImages);
     }
 }
 
-function animateJump(character_jump_images) {
+function animateJump(currentImages) {
     setTimeout(function () {
-        currentCharacterImage = character_jump_images[0];
-    }, JUMP_TIME * 2 / (character_jump_images.length * 10) * 1);
+        currentCharacterImage = currentImages[0];
+    }, JUMP_TIME * 2 / (currentImages.length * 10) * 1);
     setTimeout(function () {
-        currentCharacterImage = character_jump_images[1];
-    }, JUMP_TIME * 2 / (character_jump_images.length * 10) * 5);
+        currentCharacterImage = currentImages[1];
+    }, JUMP_TIME * 2 / (currentImages.length * 10) * 5);
     setTimeout(function () {
-        currentCharacterImage = character_jump_images[2];
-    }, JUMP_TIME * 2 / (character_jump_images.length * 10) * 25);
+        currentCharacterImage = currentImages[2];
+    }, JUMP_TIME * 2 / (currentImages.length * 10) * 25);
 }
