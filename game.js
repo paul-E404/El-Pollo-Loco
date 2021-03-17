5b2e368fceb7113a30da5059de3f48691c104eae
paul-E404 <paul_engerling@web.de>
2021-03-17 16:39:51 +0100

    Refactoring javascript code with jsDoc

--- game.js
+++ game.js
@@ -6,6 +6,8 @@ let bg_sky_x = -270;
 let bg3_ground_x = -270;
 let bg2_ground_x = -270;
 let bg1_ground_x = -270;
+let bg_ground_x_min = -10;    //Verhindert, dass character unendlich nach links laufen kann
+let bg_ground_x_max;        //Verhindert, dass character unendlich nach rechts laufen kann
 let bg_element_y = 0;
 let cloud_offset = 0;
 let isMovingRight = false;
@@ -13,13 +15,13 @@ let isMovingLeft = false;
 let lastMove = "right";
 let lastJumpStarted = new Date().getTime();
 let lastMoveFinished = new Date().getTime();
-let timePassedSinceJump;   
+let timePassedSinceJump;
 let currentCharacterImage = new Image;
 let character_idle_index = 0;
 let character_walk_index = 0;
 
 
-//Image Object
+//Image Object with all character images
 let characterImages = {
     walk: [
         ['img/character/walk/Wl-21.png',
@@ -97,7 +99,7 @@ let characterImages = {
     ]
 };
 
-//Image Object
+//Image Object with all background images
 let backgroundImages = {
     ground: [
         'img/bg/ground3/1.png',
@@ -114,33 +116,46 @@ let backgroundImages = {
     ]
 }
 
-//#############################################################################################
 
 //Ziel dieser Funktion ist es, jeden Pfadeintrag des Objektes characterImages durch das entsprechende Bildobjekt zu ersetzen und es dadurch vorzuladen.
 //Gibt jedes Element des Objekts "characterImages" aus, d.h. den Pfad.
 //x ist die Anzahl der Elemente im Objekt
 //y ist die Wahl zwischen left und right
 //z ist die Anzahl der Elemente in einem spezifischen Array, z.B. 6 Elemente im Array walk-left (da dort 6 Bilder vorhanden).
+
+/**
+ * IMAGE CACHING 
+ * Catches every entry(path) in an object which contains all image paths, and turns it into an image object.
+ * Only for objects which contain two level of arrays.
+ * @param  {obj} obj - An object which contains all image paths.
+ */
 function preloadImages(obj) {
 
-    for (let x = 0; x < Object.keys(obj).length; x++) {  
-        for(let y = 0; y < obj[Object.keys(obj)[x]].length; y++) {
+    for (let x = 0; x < Object.keys(obj).length; x++) {
+        for (let y = 0; y < obj[Object.keys(obj)[x]].length; y++) {
             for (let z = 0; z < obj[Object.keys(obj)[x]][y].length; z++) {
-                //Catch every entry (img-path) in characterImages object
+                //Catch every entry (image-path) in e.g. characterImages object
                 let path = obj[Object.keys(obj)[x]][y][z];
-                console.log("Der Pfad ist: ", path);
+                //console.log("The entry(path) is: ", path);
+                //Creates an image using the current path
                 let image = new Image();
                 image.src = path;
-                //Save entries with img-Objects back to characterImages object
+                //Save the new image object back to image collection object, e.g. characterImages object
                 obj[Object.keys(obj)[x]][y][z] = image;
             }
         }
     }
 }
 
+/**
+ * IMAGE CACHING 
+ * Catches every entry(path) in an object which contains all image paths, and turns it into an image object.
+ * Only for objects which contain one level of arrays.
+ * @param  {obj} obj - An object which contains all image paths.
+ */
 function preloadBackgroundImages(obj) {
-    for (let x = 0; x < Object.keys(obj).length; x++) {  
-        for(let y = 0; y < obj[Object.keys(obj)[x]].length; y++) {
+    for (let x = 0; x < Object.keys(obj).length; x++) {
+        for (let y = 0; y < obj[Object.keys(obj)[x]].length; y++) {
             //Catch every entry (img-path) in backgroundImages object
             let path = obj[Object.keys(obj)[x]][y];
             console.log("Der Pfad für das Backgroundimage ist: ", path);
@@ -160,7 +175,9 @@ let GAME_SPEED = 7;
 let CLOUD_SPEED = 0.2;
 
 
-//Zeichnet das Spielfeld (Startpunkt links oben)
+/**
+ * Draws the canvas and loads important start-functions.
+ */
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
@@ -172,6 +189,10 @@ function init() {
     listenForKeys();
 }
 
+
+/**
+ * Checks if the character is currently running to the right or to the left in order to prepair the correct images.
+ */
 function checkForRunning() {
     setInterval(function () {
         if (isMovingRight == true) {
@@ -185,33 +206,50 @@ function checkForRunning() {
     }, 100);
 }
 
+
+/**
+ * Iterates through the array with the running images.
+ * Calculation with modulo makes iteration from 0 to the length of the array possible
+ * @param  {Object} currentImages - Running images with right or left animation.
+ */
 function showRunningImages(currentImages) {
-    //Modulorechnung sorgt dafür, dass der Index immer von 0 bis zur Länge des Arrays durch das Array iteriert.
-    //Der Counter am Ende setzt den Index immer +1 nach oben.
-    //if(timePassedSinceJump > JUMP_TIME * 2): Damit sich Sprung und Laufanimation nicht überlagern.
+    //if(timePassedSinceJump > JUMP_TIME * 2) => no running animation while jumping!
     if (timePassedSinceJump > JUMP_TIME * 2) {
         let index = character_walk_index % currentImages.length;
         currentCharacterImage = currentImages[index];
+        //counter increase for next picture
         character_walk_index++;
     }
-
 }
 
+
+/**
+ * Checks regularly if enough time has passed for the animation of relax images or even sleep images.
+ */
 function checkForRelaxing() {
+
     setInterval(function () {
+
         let currentTime = new Date().getTime();
         let timePassedSinceLastMove = currentTime - lastMoveFinished;
+        let timeForRelax = timePassedSinceJump > JUMP_TIME * 3 && timePassedSinceJump <= JUMP_TIME * 20 || timePassedSinceLastMove > JUMP_TIME * 2 && timePassedSinceLastMove < JUMP_TIME * 20;
+        let timeForSleep = timePassedSinceJump > JUMP_TIME * 3 && timePassedSinceLastMove > JUMP_TIME * 20;
+
         if (isMovingLeft == false && isMovingRight == false) {
-            if (timePassedSinceJump > JUMP_TIME * 3 && timePassedSinceJump <= JUMP_TIME * 20 || timePassedSinceLastMove > JUMP_TIME * 2 && timePassedSinceLastMove < JUMP_TIME * 20) {
+            if (timeForRelax == true) {
                 checkRelaxingDirection();
             }
-            else if (timePassedSinceJump > JUMP_TIME * 3 && timePassedSinceLastMove > JUMP_TIME * 20) {
+            else if (timeForSleep == true) {
                 checkSleepingDirection();
             }
         }
     }, 300);
 }
 
+
+/**
+ * Decides which relax images are needed (right or left) depending on the direction of last move.
+ */
 function checkRelaxingDirection() {
     if (lastMove == "right") {
         let currentImages = characterImages['idle'][1];
@@ -223,6 +261,12 @@ function checkRelaxingDirection() {
     }
 }
 
+
+/**
+ * Iterates through the array with the relax images.
+ * Calculation with modulo makes iteration from 0 to the length of the array possible
+ * @param  {Object} currentImages - Relaxing images with right or left animation.
+ */
 function relax(currentImages) {
     let index = character_idle_index % currentImages.length;
     currentCharacterImage = currentImages[index];
@@ -230,6 +274,9 @@ function relax(currentImages) {
 }
 
 
+/**
+ * Decides which sleep images are needed (right or left) depending on the direction of last move.
+ */
 function checkSleepingDirection() {
     if (lastMove == "right") {
         let currentImages = characterImages['sleep'][1];
@@ -242,6 +289,11 @@ function checkSleepingDirection() {
 }
 
 
+/**
+ * Iterates through the array with the sleep images.
+ * Calculation with modulo makes iteration from 0 to the length of the array possible
+ * @param  {Object} currentImages - Sleeping images with right or left animation.
+ */
 function sleep(currentImages) {
     let index = character_idle_index % currentImages.length;
     currentCharacterImage = currentImages[index];
@@ -249,52 +301,79 @@ function sleep(currentImages) {
 }
 
 
+/**
+ * Draws the current background, character images and object images depending on browserspeed.
+ */
 function draw() {
     //Verhindert dass die Hintergrundbilder mehrfach angezeigt werden. Kann ggf. am Ende entfernt werden.
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     drawBackground();
     updateCharacter();
-    //requestAnimationFrame(function): Webbrowser nimmt sich immer genug Ressoucen von der Grafikkarte um diesen Frame zu aktualisieren
-    //Alternative zu setInterval, was bei einer framerate von 50 - 100 ms zu starkem Flackern führt
+    //requestAnimationFrame(function): webbrowser takes the ressources it needs from the graphic card in order to update the frame.
+    //This is a less flickering alternative to setInterval.
     requestAnimationFrame(draw);
 }
 
 
-
+/**
+ * Draws the current character image.
+ */
 function updateCharacter() {
+
     let character_image = currentCharacterImage;
 
-    //character_image.complete: Gibt den Wert true zurück, wenn das Bild fertig geladen ist. Ansonsten false.
+    checkCharacterJumpHeight();
+
+    //character_image.complete: returns true if image has finished loading
     if (character_image.complete) {
         ctx.drawImage(character_image, character_x, character_y, character_image.width * 0.35, character_image.height * 0.35);
     }
+}
 
+
+/**
+ * Regulates if the character goes up or down.
+ */
+function checkCharacterJumpHeight() {
     timePassedSinceJump = new Date().getTime() - lastJumpStarted;
+    //Check rising of the character
     if (timePassedSinceJump < JUMP_TIME) {
         character_y = character_y - 10;
     }
-    else if (timePassedSinceJump < JUMP_TIME * 2) {
-        //Check falling
+    //Check falling
+    else if (timePassedSinceJump <= JUMP_TIME * 2) {
+        //character must not fall deeper than 45
         if (character_y < 45) {
             character_y = character_y + 10;
         }
     }
 }
 
+
+/**
+ * Draws and changes the current background including sky and ground.
+ * This is important to make the character seem moving.
+ */
 function drawBackground() {
 
+    //clouds always move, independent of the character's movement.
     cloud_offset = cloud_offset + CLOUD_SPEED;
 
+    //Defines start and end of the area in which the character is able to move.
+    let wallLeft = bg1_ground_x >= bg_ground_x_min;
+    let wallRight; //follows...
+
+    //Different background layers move at different speeds and in different directions depending on the character's movement.
     if (isMovingRight == true) {
         bg3_ground_x = bg3_ground_x - GAME_SPEED * 0.2;
         bg2_ground_x = bg2_ground_x - GAME_SPEED * 0.4;
         bg1_ground_x = bg1_ground_x - GAME_SPEED;
         bg_sky_x = bg_sky_x - GAME_SPEED * 0.1;
     }
-    if (isMovingLeft == true) {
-        bg3_ground_x = bg3_ground_x + GAME_SPEED * 0.5;
-        bg2_ground_x = bg2_ground_x + GAME_SPEED * 0.8;
+    if (isMovingLeft == true && wallLeft == false) {
+        bg3_ground_x = bg3_ground_x + GAME_SPEED * 0.2;
+        bg2_ground_x = bg2_ground_x + GAME_SPEED * 0.4;
         bg1_ground_x = bg1_ground_x + GAME_SPEED;
         bg_sky_x = bg_sky_x + GAME_SPEED * 0.1;
     }
@@ -302,6 +381,10 @@ function drawBackground() {
     drawGround();
 }
 
+
+/**
+ * Draws sky and clouds.
+ */
 function drawSky() {
     for (let i = 0; i < 10; i++) {
         addBackgroundImage(backgroundImages['sky'][0], bg_sky_x, bg_element_y, i);
@@ -312,6 +395,10 @@ function drawSky() {
     }
 }
 
+
+/**
+ * Draws the three ground layers.
+ */
 function drawGround() {
     for (let i = 0; i < 10; i = i + 2) {
         addBackgroundImage(backgroundImages['ground'][0], bg3_ground_x, bg_element_y, i);
@@ -327,22 +414,39 @@ function drawGround() {
     }
 }
 
+
+/* 
+* Draws a background image.
+*/
 function addBackgroundImage(image, bg_element_x, bg_element_y, scale) {
     if (image.complete) {
         ctx.drawImage(image, bg_element_x + canvas.width * scale, bg_element_y, canvas.width, canvas.height);
     }
 }
 
-//Registriert wenn eine Taste gedrückt wird und kann dann eine Funktion aufrufen.
+
+/**
+ * Listens for keys being pressed or released.
+ */
 function listenForKeys() {
     //e steht für event
     //Das Event (Keyboardevent) ist ein JSON, in dem sich alle Informationen zu einer gedrückten Taste befinden.
     //Um sich das komplette Event (JSON) anzeigen zu lassen
     //console.log(e);
+    keyDown();
+    keyUp();
+
+}
+
+
+/**
+ * Listens for keys being pressed.
+ */
+function keyDown() {
+    //If a special key is pressed
     document.addEventListener('keydown', function (e) {
         //e.key bedeutet, ich möchte von dem JSON Event des keys namens "code" den value und dieser ist "ArrowRight" etc.
         let key = e.code; // z.B. "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
-        //Wenn die rechte Steuerungstaste gedrückt wird, möchten wir die Koordinaten von unserem character updaten
         if (key == "ArrowRight") {
             isMovingRight = true;
             lastMove = "right";
@@ -362,11 +466,16 @@ function listenForKeys() {
             lastJumpStarted = new Date().getTime();         //Unix Timestamp
             timePassedSinceLastMove = new Date().getTime();
             checkJumpDirection();
-
         }
     });
+}
 
-    //Wenn die Taste losgelassen wird...
+
+/**
+ * Listens for keys being released.
+ */
+function keyUp() {
+    //If a special key is released
     document.addEventListener('keyup', function (e) {
         let key = e.code;
         if (key == "ArrowRight") {
@@ -380,9 +489,12 @@ function listenForKeys() {
             /* character_x = character_x - 5; */
         }
     });
-
 }
 
+
+/**
+ * Decides which jump images are needed (right or left) depending on the direction of last move.
+ */
 function checkJumpDirection() {
     if (lastMove == "right") {
         let currentImages = characterImages['jump'][1];
@@ -394,6 +506,11 @@ function checkJumpDirection() {
     }
 }
 
+
+/**
+ * Iterates through the jump images during the time of one full jump.
+ * @param  {Object} currentImages - Jump images with right or left animation.
+ */
 function animateJump(currentImages) {
     setTimeout(function () {
         currentCharacterImage = currentImages[0];
